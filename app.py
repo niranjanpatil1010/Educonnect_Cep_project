0d66a7b1a93f0bdcfeaef033bcb490f21581caf8
niranjanpatil1010 <niranjanpatil4550@gmail.com>
2026-07-04 15:27:06 +0530

    Add new  files

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 import db
-import os
 
 # Import Blueprints
 from routes.auth_routes import auth_bp
@@ -72,6 +71,4 @@ def health():
     return jsonify({"status": "ok", "message": "Community Platform Flask API is running"})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=False)
-    
+    app.run(port=5000, debug=True)
